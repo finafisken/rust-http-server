9cac468e98731d9062b8d78523d5b8c7ff9b31ca
Markus Warne <markus.warne@gmail.com>
2022-10-28 16:24:53 +0200

    basic routing

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,34 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
 
     // println!("Req: {:#?}", http_request);
-    let http_header = "HTTP/1.1 200 OK";
-    let body = fs::read_to_string("test.html").unwrap();
-    let length = body.len();
 
-    let response = format!("{http_header}\r\nContent-Length: {length}\r\n\r\n{body}");
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let response = match &request_line[..] {
+        "GET / HTTP/1.1" => {
+            let http_header = "HTTP/1.1 200 OK";
+            let body = fs::read_to_string("test.html").unwrap();
+            build_response_string(http_header, &body)
+        }
+        _ => {
+            let http_header = "HTTP/1.1 404 NOT_FOUND";
+            build_response_string(http_header, "")
+        }
+    };
+    // let http_header = "HTTP/1.1 200 OK";
+    // let body = fs::read_to_string("test.html").unwrap();
+    // let response = build_response_string(http_header, &body);
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn build_response_string(header: &str, body: &str) -> String {
+    let length = body.len();
+    format!("{header}\r\nContent-Length: {length}\r\n\r\n{body}")
 }
